577fa58d7a668536365afde0eb56f48f9322d1b1
SamiGaming <104452588+SamiGamin@users.noreply.github.com>
2023-10-13 14:58:13 -0500

    actualizacion 13/10/2013

--- app/src/main/java/com/greengarden/MainActivity.java
+++ app/src/main/java/com/greengarden/MainActivity.java
@@ -1,8 +1,15 @@
 package com.greengarden;
-
+/*
+  @Autor: @Sami
+  @Creacion: 13/10/2023
+  @Modificacion:
+  @Descripcion: pantalla de inicio 4 seg
+  */
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent pantallaLogin = new Intent(getApplicationContext(), Login.class);
+                startActivity(pantallaLogin);
+                finish();
+            }
+        }, 0);
     }
 }
