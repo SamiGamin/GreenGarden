7167315e8b3c80e03e44acc9ecf091149e7f18ca
SamiGaming <104452588+SamiGamin@users.noreply.github.com>
2023-10-13 20:02:15 -0500

    base de datos  13/10/2013

--- app/src/main/java/com/greengarden/Registration.java
+++ app/src/main/java/com/greengarden/Registration.java
@@ -2,27 +2,39 @@ package com.greengarden;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.greengarden.datos.DatePickerHelper;
-import com.greengarden.datos.DbConntactos;
-import com.greengarden.datos.DbHelper;
+import com.greengarden.datos.BseDatos;
 import com.greengarden.datos.SelectorOpcion;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.regex.Pattern;
 
 public class Registration extends AppCompatActivity {
 
-    TextView registro,crearbase, nombre,fecha_de_nacimiento,corre_eletronico,usuario,contrasena,google;
+    TextView registro;
+    EditText nombre_usu,
+            fecha_de_nacimiento,
+            corre_eletronico, usuario,
+            contrasena, corfirmarContraseña;
     Spinner genero;
     CheckBox checkBox;
+    BseDatos miBdd;
     private TextView editTextFechaNacimiento;
     private DatePickerHelper datePickerHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,60 +45,138 @@ public class Registration extends AppCompatActivity {
         SelectorOpcion selectorOpcion = new SelectorOpcion(spinner);
 
         registro = findViewById(R.id.reg_btn_Registrarse);
-        crearbase = findViewById(R.id.crear_base_dt);
-
-        nombre = findViewById(R.id.rg_name);
+        corfirmarContraseña = findViewById(R.id.editcofircontrasena);
+        nombre_usu = findViewById(R.id.rg_name);
         fecha_de_nacimiento = findViewById(R.id.rg_fecha);
         corre_eletronico = findViewById(R.id.reg_editText_email);
         usuario = findViewById(R.id.reg_editText_usuario);
         contrasena = findViewById(R.id.editTextcontrasena);
-        google = findViewById(R.id.google);
         genero = findViewById(R.id.spinner);
         checkBox = findViewById(R.id.checkBox);
-
-        crearbase.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                 DbHelper dbHelper = new DbHelper(Registration.this);
-                 SQLiteDatabase db = dbHelper.getWritableDatabase();
-                 if (db != null){
-                 Toast.makeText(Registration.this, "BASE DE DATOS CREADA", Toast.LENGTH_SHORT).show();
-                 }else {
-                 Toast.makeText(Registration.this, "ERROR AL CREAR BASE DE DATOS", Toast.LENGTH_SHORT).show();
-                 }
-            }
-        });
-
-
-
-        //guardatos
-        registro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-               DbConntactos dbConntactos = new DbConntactos(Registration.this);
-                long id = dbConntactos.insertarContactos(nombre.getText().toString(),fecha_de_nacimiento.getText().toString(),
-                        corre_eletronico.getText().toString(),usuario.getText().toString(),
-                        contrasena.getText().toString());
-                if (id > 0){
-                    Toast.makeText(Registration.this, "REGISTRO GUARDADO", Toast.LENGTH_SHORT).show();
-                    limpiar();
-                }else {
-                    Toast.makeText(Registration.this, "ERROR AL GUARDAR REGISTRO", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        miBdd = new BseDatos(getApplicationContext());
     }
 
+    public void registrarUsuario(View vista) {
 
+        String nombre = nombre_usu.getText().toString();
+        String fechaN = fecha_de_nacimiento.getText().toString();
+        String email = corre_eletronico.getText().toString();
+        String Usuario = usuario.getText().toString();
+        String password = contrasena.getText().toString();
+        String passwordConfirmada = corfirmarContraseña.getText().toString();
+        // String Genero =genero.get().toString();
+        DateFormat df = new SimpleDateFormat("EEE, d MMM yyyy, HH:mm");
+        String fecha_registro = df.format(Calendar.getInstance().getTime());
+        if (!checkBox.isChecked()){
+            Toast.makeText(this, "Por favor aceptar terminos y condiciones  ", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (nombre.isEmpty() || fechaN.isEmpty() || email.isEmpty() || Usuario.isEmpty() || password.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Para continuar con el registro llene todos los campos solicitados", Toast.LENGTH_SHORT).show();
 
-    private void limpiar() {
-        nombre.setText("");
-        fecha_de_nacimiento.setText("");
-        corre_eletronico.setText("");
-        usuario.setText("");
-       contrasena.setText("");
+        } else {
+            if (contienSololetras(nombre) == false) {
+                Toast.makeText(this, "El nombre no debe contener numeros", Toast.LENGTH_SHORT).show();
+            } else {
+                Pattern pattern = Patterns.EMAIL_ADDRESS;
+                if (pattern.matcher(email).matches() == false) {
+                    Toast.makeText(this, "Ingrese un Email Valido", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (validarUsuario(Usuario) == false) {
+                        Toast.makeText(this, "Ingrese un usuario minimo de 4 digitos", Toast.LENGTH_SHORT).show();
+                    }
+                    if (password.length() < 6) {
+                        Toast.makeText(this, "Ingrese una contraseña minimo de 6 digitos", Toast.LENGTH_SHORT).show();
+                    } else {
+                        if (validarpassword(password) == false) {
+                            Toast.makeText(this, "La contraseña debe tener numeros y letras", Toast.LENGTH_SHORT).show();
+                        } else {
+                            if (password.equals(passwordConfirmada)) {
+                                password = getMD5(password);
+                                miBdd.agregarUsuario(nombre, fechaN, email, password, fecha_registro);
+                                Toast.makeText(this, "Usuario registrado correctamente", Toast.LENGTH_SHORT).show();
+                                finish();
+                            } else {
+                                Toast.makeText(this, "Las contraseñas ingresadas no considen ", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
+    private boolean validarUsuario(String usuario) {
+        boolean numeros = false;
+        boolean letras = false;
+        for (int x = 0; x < usuario.length(); x++) {
+            char c = usuario.charAt(x);
+            if (((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == ' ')
+                    || (c == 'ñ') || (c == 'Ñ') || (c == 'Á') || (c == 'á') ||
+                    (c == 'é') || (c == 'É') || (c == 'í') ||
+                    (c == 'Í') || (c == 'ó') || (c == 'Ó') ||
+                    (c == 'ú') || (c == 'Ú'))) {
+                letras = true;
+            }
+            if ((c >= '0' && c <= '9')) {
+                numeros = true;
+            }
+        }
+        if (numeros == true && letras == true) {
+            return true;
+        }
+        return false;
+    }
+
+    private String getMD5(String password) {
+        try {
+            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+            digest.update(password.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            StringBuilder hexString = new StringBuilder();
+            for (byte aMessageDigest : messageDigest)
+                hexString.append(Integer.toHexString(0xFF & aMessageDigest));
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private boolean validarpassword(String password) {
+        boolean numeros = false;
+        boolean letras = false;
+        for (int x = 0; x < password.length(); x++) {
+            char c = password.charAt(x);
+            if (((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == ' ')
+                    || (c == 'ñ') || (c == 'Ñ') || (c == 'Á') || (c == 'á') ||
+                    (c == 'é') || (c == 'É') || (c == 'í') ||
+                    (c == 'Í') || (c == 'ó') || (c == 'Ó') ||
+                    (c == 'ú') || (c == 'Ú'))) {
+                letras = true;
+            }
+            if ((c >= '0' && c <= '9')) {
+                numeros = true;
+            }
+        }
+        if (numeros == true && letras == true) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean contienSololetras(String nombre) {
+        for (int x = 0; x < nombre.length(); x++) {
+            char c = nombre.charAt(x);
+            if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == ' ')
+                    || (c == 'ñ') || (c == 'Ñ') || (c == 'Á') || (c == 'á') ||
+                    (c == 'é') || (c == 'É') || (c == 'í') || (c == 'Í') || (c == 'ó')
+                    || (c == 'Ó') || (c == 'ú') || (c == 'Ú'))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
---
+++ app/src/main/java/com/greengarden/datos/BseDatos.java
@@ -0,0 +1,53 @@
+package com.greengarden.datos;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class BseDatos extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NOMBRE = "Usuarios";
+    public static final String TABLE_USUARIOS = "create table usuario(id_usu integer primary key autoincrement, nombre_completo_usu text," +
+            "fecha_nacimiento_sus text, genero_usu text, email_usu text,  passward_usu text, fecha_regis_usu text)";
+
+    public BseDatos(Context context) {
+        super(context, DATABASE_NOMBRE, null, DATABASE_VERSION);
+    }
+
+    public BseDatos(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version, @Nullable DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    /**public BseDatos(@Nullable Context context, @Nullable String name, int version, @NonNull SQLiteDatabase.OpenParams openParams) {
+        super(context, name, version, openParams);
+    }**/
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_USUARIOS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS usurio");
+        db.execSQL(TABLE_USUARIOS);
+    }
+
+    public boolean agregarUsuario(String nombre_completo_usu, String fecha_nacimiento_sus,  String email_usu, String passward_us, String fecha_regis_us) {
+        SQLiteDatabase miBdd = getWritableDatabase();
+        if (miBdd != null) {
+            miBdd.execSQL("insert into usuario(nombre_completo_usu, fecha_nacimiento_sus, genero_usu,email_usu,passward_us,fecha_regis_us) " +
+                    "values('" + nombre_completo_usu + "','" + fecha_nacimiento_sus + "','" + email_usu +
+                    "','" + passward_us + "','" + fecha_regis_us + "')");
+            miBdd.close();
+            return true;
+        }
+        return false;
+    }
+}
+
