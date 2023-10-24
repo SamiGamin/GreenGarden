6072422db6590342694110da003372924b551fea
SamiGaming <104452588+SamiGamin@users.noreply.github.com>
2023-10-23 20:02:07 -0500

    menu en pantalla de inicio y en cultivos
     22/10/2013

--- app/src/main/java/com/greengarden/Login.java
+++ app/src/main/java/com/greengarden/Login.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
-    TextView Registro, ingreso;
+    Button ingreso;
+    TextView Registro, recuperarcontrasena;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,15 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.login);
         Registro = findViewById(R.id.long_tex_reduistrate);
         ingreso = findViewById(R.id.long_btn_ingresar);
+        recuperarcontrasena = findViewById(R.id.btn_recuperarcontrasena);
+        recuperarcontrasena.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Registro = new Intent(Login.this, RecuperarContrasena.class);
+                startActivity(Registro);
+                finish();
+            }
+        });
         ingreso.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
