e8e902157301be57ba1bdd2837d98da7dd74da13
SamiGaming <104452588+SamiGamin@users.noreply.github.com>
2023-10-30 20:15:12 -0500

    terminos y condiciones
     25/10/2013

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,13 +35,15 @@ android {
 dependencies {
     implementation ("androidx.core:core-splashscreen:1.0.1")
     //navegacion
-    implementation ("androidx.navigation:navigation-fragment:2.3.5")
-    implementation ("androidx.navigation:navigation-ui:2.3.5")
+    implementation ("androidx.navigation:navigation-fragment:2.7.4")
+    implementation ("androidx.navigation:navigation-ui:2.7.4")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-auth:22.1.2")
+    implementation("com.google.firebase:firebase-auth:22.2.0")
+    implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
