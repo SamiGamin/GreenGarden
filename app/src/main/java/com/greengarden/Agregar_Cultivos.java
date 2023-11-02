1af41d568b75176d2605c82331908f65a0846db6
dasdo <ingcpu@gmail.com>
2023-11-02 17:51:33 -0500

    se agrego spinner

--- app/src/main/java/com/greengarden/Agregar_Cultivos.java
+++ app/src/main/java/com/greengarden/Agregar_Cultivos.java
@@ -3,31 +3,114 @@ package com.greengarden;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.PopupMenu;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.greengarden.Cultivo.huerto;
+
+import java.util.ArrayList;
+
 public class Agregar_Cultivos extends AppCompatActivity {
-    Button menu, ircuidados;
+
+    private Button buttonNavigate;
+    private Spinner spinnerProducts;
+    private Button buttonAddToCart;
+    private ListView listViewCart;
+    private ArrayList<String> cartItems;
+    private ArrayAdapter<String> cartAdapter;
+
+
+    Button menu, Btnhuerto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.agregar_cultivos);
-        menu = findViewById(R.id.btn_menu);
-        ircuidados = findViewById(R.id.btn_agregar_cultivo);
+        buttonNavigate = findViewById(R.id.buttonNavigate);
 
-        ircuidados.setOnClickListener(new View.OnClickListener() {
+
+        // Obtener referencias de los elementos de la interfaz
+        TextView textViewTitle = findViewById(R.id.textViewTitle);
+        spinnerProducts = findViewById(R.id.spinnerProducts);
+        buttonAddToCart = findViewById(R.id.buttonAddToCart);
+        TextView textViewCart = findViewById(R.id.textViewCart);
+        listViewCart = findViewById(R.id.listViewCart);
+
+        // Configurar el título y los productos del catálogo
+        textViewTitle.setText("Agregar nuevas plantas al Huerto");
+        String[] products = {"Plantas en el Huerto", "Zanahoria", "Ajo", "Remolacha", "Lechuga", "Acelga", "Albahaca", "Hierbabuena", "Hinojo", "Fastron", "Laurel"};
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, products);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerProducts.setAdapter(spinnerAdapter);
+
+        // Configurar el carrito de compras
+        cartItems = new ArrayList<>();
+        cartAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cartItems);
+        listViewCart.setAdapter(cartAdapter);
+
+
+        menu = findViewById(R.id.btn_menu);
+        Btnhuerto = findViewById(R.id.buttonNavigate);
+
+        buttonAddToCart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent ircuidados = new Intent(Agregar_Cultivos.this , Cuidados.class);
+                String selectedItem = spinnerProducts.getSelectedItem().toString();
+                SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+                int totalElementos = sharedPreferences.getInt("totalElementos", 0);
+                boolean alreadyAdded = false;
+                for (int i = 1; i <= totalElementos; i++) {
+                    String seleccion = sharedPreferences.getString("seleccion" + i, "");
+                    if (seleccion.equals(selectedItem)) {
+                        alreadyAdded = true;
+                        break;
+                    }
+                }
+                if (!alreadyAdded) {
+                    cartItems.add(selectedItem);
+                    cartAdapter.notifyDataSetChanged();
+                    Toast.makeText(getApplicationContext(), "Planta agregada al huerto", Toast.LENGTH_SHORT).show();
+                    // Guardar la selección en SharedPreferences
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("seleccion" + (totalElementos + 1), selectedItem);
+                    editor.putInt("totalElementos", totalElementos + 1);
+                    editor.apply();
+                } else {
+                    Toast.makeText(getApplicationContext(), "La planta ya está en el huerto", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        buttonNavigate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Agregar_Cultivos.this, huerto.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Btnhuerto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ircuidados = new Intent(Agregar_Cultivos.this, huerto.class);
                 startActivity(ircuidados);
             }
         });
+
+
+        //comienso de menu
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,7 +128,8 @@ public class Agregar_Cultivos extends AppCompatActivity {
                         if (id == R.id.inicio) {
                             Intent inicio = new Intent(Agregar_Cultivos.this, Inicio.class);
                             startActivity(inicio);
-                            return true;}
+                            return true;
+                        }
                         if (id == R.id.consumo) {
 
                             Intent consumo = new Intent(Agregar_Cultivos.this, Cuidados.class);
@@ -55,22 +139,27 @@ public class Agregar_Cultivos extends AppCompatActivity {
                         if (id == R.id.estadisticas) {
                             Intent estadisticas = new Intent(Agregar_Cultivos.this, Estadisticas.class);
                             startActivity(estadisticas);
-                            return true;}
+                            return true;
+                        }
                         if (id == R.id.consejos) {
                             Intent consejos = new Intent(Agregar_Cultivos.this, Consejos.class);
                             startActivity(consejos);
-                            return true;}
+                            return true;
+                        }
                         if (id == R.id.cultivos) {
                             Intent cultivos = new Intent(Agregar_Cultivos.this, Agregar_Cultivos.class);
                             startActivity(cultivos);
-                            return true;}
+                            return true;
+                        }
                         if (id == R.id.comu_even) {
                             Intent eventos = new Intent(Agregar_Cultivos.this, ComunidadEventos.class);
                             startActivity(eventos);
-                            return true;}
+                            return true;
+                        }
                         if (id == R.id.salir) {
                             finish();
-                            return true;}
+                            return true;
+                        }
 
                         return false;
                     }
@@ -78,5 +167,7 @@ public class Agregar_Cultivos extends AppCompatActivity {
                 popupMenu.show();
             }
         });
+        // fin de menu
+
     }
 }
---
+++ app/src/main/java/com/greengarden/Cultivo/huerto.java
@@ -0,0 +1,126 @@
+package com.greengarden.Cultivo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.greengarden.Plantas.ajo;
+import com.greengarden.Plantas.zanahoria;
+import com.greengarden.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class huerto extends AppCompatActivity {
+    private ListView listViewSeleccionados;
+    private ArrayList<String> seleccionadosList;
+    private ArrayAdapter<String> seleccionadosAdapter;
+    private SharedPreferences sharedPreferences;
+    private Map<String, Class<?>> actividadMap;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.huerto);
+
+        // Obtener referencia de la ListView
+        listViewSeleccionados = findViewById(R.id.listViewSeleccionados);
+
+        // Obtener todas las selecciones guardadas en SharedPreferences
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        seleccionadosList = new ArrayList<>();
+
+        // Obtener el número total de elementos en SharedPreferences
+        int totalElementos = sharedPreferences.getInt("totalElementos", 0);
+
+        // Recorrer los elementos y agregarlos a la lista
+        for (int i = 1; i <= totalElementos; i++) {
+            String seleccion = sharedPreferences.getString("seleccion" + i, "");
+            seleccionadosList.add(seleccion);
+        }
+
+        // Configurar el adaptador y mostrar las selecciones en la lista
+        seleccionadosAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, seleccionadosList);
+        listViewSeleccionados.setAdapter(seleccionadosAdapter);
+
+        // Configurar el CheckBox para eliminar elementos
+        CheckBox checkBoxEliminar = findViewById(R.id.checkBoxEliminar);
+        checkBoxEliminar.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // Habilitar el modo de eliminación
+                    listViewSeleccionados.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            // Obtener el elemento seleccionado en la posición
+                            String elementoSeleccionado = seleccionadosList.get(position);
+
+                            // Eliminar el elemento de la lista
+                            seleccionadosList.remove(position);
+
+                            // Actualizar los valores en SharedPreferences
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.remove("seleccion" + (position + 1));
+                            editor.putInt("totalElementos", seleccionadosList.size());
+                            for (int i = 0; i < seleccionadosList.size(); i++) {
+                                editor.putString("seleccion" + (i + 1), seleccionadosList.get(i));
+                            }
+                            editor.apply();
+
+                            // Notificar al adaptador que los datos han cambiado
+                            seleccionadosAdapter.notifyDataSetChanged();
+
+                            // Mostrar un mensaje indicando que el elemento ha sido eliminado
+                            Toast.makeText(getApplicationContext(), "Elemento eliminado: " + elementoSeleccionado, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    // Deshabilitar el modo de eliminación
+                    listViewSeleccionados.setOnItemClickListener(null);
+                }
+            }
+        });
+
+        // Crear el mapa de actividades correspondientes a cada elemento seleccionado
+        actividadMap = new HashMap<>();
+        actividadMap.put("Zanahoria", zanahoria.class);
+        actividadMap.put("Ajo", ajo.class);
+        // Agrega más elementos y actividades según sea necesario
+
+        // Agregar un listener para cuando se seleccione un elemento en la lista
+        listViewSeleccionados.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Obtener el elemento seleccionado en la posición
+                String elementoSeleccionado = seleccionadosList.get(position);
+
+                // Obtener la actividad correspondiente al elemento seleccionado
+                Class<?> actividad = actividadMap.get(elementoSeleccionado);
+
+                // Verificar si se encontró una actividad correspondiente
+                if (actividad != null) {
+                    // Crear un Intent para iniciar la nueva actividad
+                    Intent intent = new Intent(huerto.this, actividad);
+
+                    // Iniciar la nueva actividad
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Lo Siento no tenemos Informacion sobre Esa Planta", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
