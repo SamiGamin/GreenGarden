d6d5763d108aa1d8c9a779056d8bf6433b6f33c3
SamiGaming <104452588+SamiGamin@users.noreply.github.com>
2023-11-11 16:42:20 -0500

    ya se puede agreagr la cantidad de plantas con aler personalisado
     09/11/2013

--- app/src/main/java/com/greengarden/Listadoplantas/Adapter/PlantaAdapter.java
+++ app/src/main/java/com/greengarden/Listadoplantas/Adapter/PlantaAdapter.java
@@ -1,7 +1,10 @@
 package com.greengarden.Listadoplantas.Adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.text.InputType;
 import android.text.TextUtils;
 import android.util.Log;
@@ -12,6 +15,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -28,13 +32,6 @@ public class PlantaAdapter extends RecyclerView.Adapter<PlantaAdapter.MiViewHold
     private final Context context;
     private final ArrayList<Tituloplanta> tituloplanta;
     private final ArrayList<Tituloplanta> selectedTitles;
-
-    private int Position = RecyclerView.NO_POSITION;
-    public void setPosition(int position) {
-        this.Position = position;
-        notifyDataSetChanged();
-    }
-
 
     public PlantaAdapter(Context context, ArrayList<Tituloplanta> tituloplanta, ArrayList<Tituloplanta> selectedTitles) {
         this.context = context;
@@ -57,7 +54,7 @@ public class PlantaAdapter extends RecyclerView.Adapter<PlantaAdapter.MiViewHold
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MiViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MiViewHolder holder, @SuppressLint("RecyclerView") int position) {
         Tituloplanta planta = tituloplanta.get(position);
 
         holder.Titulo.setText(planta.getTitulo());
@@ -75,7 +72,7 @@ public class PlantaAdapter extends RecyclerView.Adapter<PlantaAdapter.MiViewHold
                     .into(holder.plantaimagen);
         } else {
 
-            // holder.plantaimagen.setImageURI(R.drawable.dangerous);
+
         }
 
         // Cambia el fondo del elemento si está seleccionado
@@ -88,7 +85,7 @@ public class PlantaAdapter extends RecyclerView.Adapter<PlantaAdapter.MiViewHold
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mostrarDialogoCantidad(planta);
+                mostrarDialogoCantidad(planta, position);
                 if (selectedTitles.contains(planta)) {
                     selectedTitles.remove(planta);
                     holder.itemView.setBackgroundColor(context.getResources().getColor(android.R.color.transparent));
@@ -102,21 +99,26 @@ public class PlantaAdapter extends RecyclerView.Adapter<PlantaAdapter.MiViewHold
         holder.cantidaplanta.setText(String.valueOf(planta.getCantidad()));
 
     }
-    private AlertDialog alertDialog;
+
     // Método para mostrar el cuadro de diálogo de cantidad
-    private void mostrarDialogoCantidad(Tituloplanta planta) {
+    private void mostrarDialogoCantidad(Tituloplanta planta, int position) {
         LayoutInflater inflater = LayoutInflater.from(context);
         View dialogView = inflater.inflate(R.layout.custom_dialog, null);
         // Configura el AlertDialog
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setView(dialogView);
-        //builder.setTitle("Ingrese la cantidad de Plantas " + planta.getTitulo());
+        // Evita que el AlertDialog se cierre al tocar fuera de él
+        builder.setCancelable(false);
 
         // Encuentra las vistas dentro del diseño personalizado
         EditText editTextCantidad = dialogView.findViewById(R.id.editTextCantidad);
         Button btnAceptar = dialogView.findViewById(R.id.btnAceptar);
         // Crea y muestra el AlertDialog
         AlertDialog alertDialog = builder.create();
+
+        // Añade una vista transparente como fondo para evitar el cierre al tocar fuera
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
         alertDialog.show();
 
         btnAceptar.setOnClickListener(new View.OnClickListener() {
@@ -124,45 +126,17 @@ public class PlantaAdapter extends RecyclerView.Adapter<PlantaAdapter.MiViewHold
             public void onClick(View v) {
                 // Obtiene la cantidad ingresada por el usuario
                 String cantidadIngresada = editTextCantidad.getText().toString();
-                planta.setCantidad(Integer.parseInt(cantidadIngresada));
+                if (cantidadIngresada.isEmpty()){
+                    Toast.makeText(context, "Debes ingresar la cantidad", Toast.LENGTH_SHORT).show();
+                }else {
+                    planta.setCantidad(Integer.parseInt(cantidadIngresada));
 
-                // Cierra el AlertDialog
-                alertDialog.dismiss();
-                notifyItemChanged(Position);
-            }
-        });
-
-
-
-        // Configurar el cuadro de texto de entrada para la cantidad
-        /*final EditText input = new EditText(context);
-        input.setInputType(InputType.TYPE_CLASS_NUMBER);
-        builder.setView(input);*/
-
-       /* // Configurar los botones del cuadro de diálogo
-        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                // Obtener la cantidad ingresada por el usuario
-                String cantidadStr = input.getText().toString();
-                if (!TextUtils.isEmpty(cantidadStr)) {
-                    int cantidad = Integer.parseInt(cantidadStr);
-                    // Actualizar la cantidad en el objeto Tituloplanta
-                    planta.setCantidad(cantidad);
-                    // Notificar cambios en el conjunto de datos
-                    notifyDataSetChanged();
+                    // Cierra el AlertDialog
+                    alertDialog.dismiss();
+                    notifyItemChanged(position);
                 }
             }
-        });*/
-
-        /*builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
         });
-
-        builder.show();*/
     }
 
     @Override
